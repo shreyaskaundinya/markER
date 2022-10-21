ab6a316eb80b50064d389e00f51cd2b823b0ef60
Shreyas Kaundinya <shreyassk08@gmail.com>
2022-10-21 11:23:06 +0530

    Added test function to test lexing

---
+++ backend/tests/lexer_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	json "encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	fmt.Println("TESTING PING")
+	requestUrl := "http://localhost:8080/ping"
+	res, err := http.Get(requestUrl)
+
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+}
+
+
+
+func TestLexer(t *testing.T) {
+	fmt.Println("TESTING LEXER")
+	requestUrl := "http://localhost:5050/lex"
+
+	f, err := os.Open("./sample_files/sample_diagram.txt")
+	
+	if err != nil {
+		fmt.Printf("error opening file: %s\n", err)
+		os.Exit(1)
+	}
+
+	b := make([]byte, 1000)
+	f.Read(b)
+	s := string(b[:])
+
+	j := map[string]string{
+		"code": s,
+	}
+
+	jsonData, _ := json.Marshal(j)
+
+	request, _ := http.NewRequest("POST", requestUrl, bytes.NewBuffer(jsonData))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, error := client.Do(request)
+	if error != nil {
+		panic(error)
+	}
+	defer response.Body.Close()
+
+	fmt.Println("response Status:", response.Status)
+	fmt.Println("response Headers:", response.Header)
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println("response Body:", string(body))
+
+}
